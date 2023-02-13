e0e6e7f392dbc85c1e427cbbcaef93bfabcc829e
btuhany <114235184+btuhany@users.noreply.github.com>
2023-02-14 00:09:12 +0300

    Rigidbody jumping and animations

--- Platformer2D/Assets/GameFolders/Scripts/Concretes/PlayerController.cs
+++ Platformer2D/Assets/GameFolders/Scripts/Concretes/PlayerController.cs
@@ -7,9 +7,16 @@ namespace Controllers
     public class PlayerController : MonoBehaviour
     {
         [SerializeField] float _moveSpeed = 2f;
+        [SerializeField] private float _jumpForce = 5f;
+        bool _isJumped;
+
+
         Animator _anim;
+        Rigidbody2D _rb;
+
         private void Awake()
         {
+            _rb = GetComponent<Rigidbody2D>();
             _anim= GetComponent<Animator>();
         }
         private void Update()
@@ -17,9 +24,23 @@ namespace Controllers
             float horizontal = Input.GetAxis("Horizontal");
             _anim.SetFloat("moveSpeed", Mathf.Abs(horizontal));
             transform.Translate(Vector2.right * horizontal * _moveSpeed * Time.deltaTime);
-            if(horizontal != 0)
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                _isJumped = true;
+                
+            }
+            if (horizontal != 0)
             {
                 transform.localScale = new Vector3(Mathf.Sign(horizontal),transform.localScale.y, transform.localScale.z);
+            }
+        }
+        private void FixedUpdate()
+        {
+            if(_isJumped)
+            {
+                
+                _rb.AddForce(Vector2.up * _jumpForce );
+                _isJumped = false;
             }
         }
     }
