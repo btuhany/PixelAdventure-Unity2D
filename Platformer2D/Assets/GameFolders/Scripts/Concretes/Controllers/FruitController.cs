82e615d576406401ba5c84dee4c50558094db913
btuhany <114235184+btuhany@users.noreply.github.com>
2023-02-22 15:26:39 +0300

    Banana ve pineapple

--- Platformer2D/Assets/GameFolders/Scripts/Concretes/Controllers/FruitController.cs
+++ Platformer2D/Assets/GameFolders/Scripts/Concretes/Controllers/FruitController.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using static Fruits;
 
 public class FruitController : MonoBehaviour
 {
+    [SerializeField] FruitType _fruitType;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
---
+++ Platformer2D/Assets/GameFolders/Scripts/Concretes/Enum/Fruits.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruits 
+{
+    public enum FruitType
+    {
+        Melon,
+        Pineapple,
+        Banana
+    }
+}
